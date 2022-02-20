69fc13c2b2759e511ad7ffb7a481fcdcd246a70a
throughhyej <hyejeyh@gmail.com>
2022-02-20 10:46:27 +0900

    회원 관리: FrontController V3 - Servlet 종속성 제거, View Resolver 구현

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
 
@@ -17,6 +18,16 @@ public class MyView {
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(path);
         dispatcher.forward(request, response);
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response, Map<String, Object> model) throws ServletException, IOException {
+        modelToRequestAttribute(request, model);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+        dispatcher.forward(request, response);
+    }
+
+    private void modelToRequestAttribute(HttpServletRequest request, Map<String, Object> model) {
+        model.forEach((name, value) -> request.setAttribute(name, value));
     }
 
 }
